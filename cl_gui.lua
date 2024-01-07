a2f9c545ee593168df8b9c45c1c5c814ec0fbf9b
Eliott-B <eliottb.info@gmail.com>
2024-01-07 15:35:25 +0100

    :rocket: | Deploy GUI

---
+++ cl_gui.lua
@@ -0,0 +1,24 @@
+function Draw2DText(x, y, text, scale)
+    SetTextFont(4)
+    SetTextProportional(7)
+    SetTextScale(scale, scale)
+    SetTextColour(255, 255, 255, 255)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextDropShadow()
+    SetTextEdge(4, 0, 0, 0, 255)
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x, y)
+end
+
+function SirenOn()
+    sirenIndication = Draw2DText(0.95, 0.94, "Siren : ~g~On", 0.45)
+end
+
+function SirenOff()
+    sirenIndication = Draw2DText(0.95, 0.94, "Siren : ~r~Off", 0.45)
+end
+
+function LightOff()
+    sirenIndication = Draw2DText(0.95, 0.94, "Light : ~r~Off", 0.45)
+end
