4c5843f56612efc6c313aab483cb79b85a33ec34
JellyPh1sh <sacha.sorgiati@gmail.com>
2024-01-14 17:44:47 +0100

    :rocket: | Deploy REACTJS in script.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,24 @@
 fx_version 'cerulean'
 game 'gta5'
 
+lua54 "yes"
+
 author 'Eliott-B'
 description 'Emergency siren manager'
 version '1.0.0'
 
-client_script {
+client_scripts {
     "client/cl_siren.lua",
     "client/cl_gui.lua"
 }
 
-server_script {
+server_scripts {
     "server/sv_sync.lua"
+}
+
+ui_page "web/build/index.html"
+
+files {
+    "web/build/index.html",
+    "web/build/**/*"
 }
